5ddd8a5cae003e1128bd8c5ab61de130a91c5c23
Marc0s-ikp <marcus.ikpase@gmail.com>
2025-10-24 13:22:21 +0100

    Enhance counter simulation with Vbuddy integration and add configuration file

--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -2,6 +2,7 @@
 #include "Vcounter.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
 
 int main(int argc, char **argv, char **env) {
 
@@ -17,6 +18,10 @@ int main(int argc, char **argv, char **env) {
     top->trace(tfp, 99);
     tfp ->open("counter.vcd");
 
+    // init Vbuddy
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 1: Counter");
+
     // initialize simulation inputs
     top->clk = 1;
     top->rst = 1;
@@ -31,10 +36,21 @@ int main(int argc, char **argv, char **env) {
             top->clk = !top->clk; //toggle clock
             top->eval(); //evaluate model
         }
+
+        // Vbuddy output
+        vbdHex(4, (init(top->count) >> 16) & 0xF); // upper 4 bits
+        vbdHex(3, (init(top->count) >> 8) & 0xF);
+        vbdHex(2, (init(top->count) >> 4) & 0xF);
+        vbdHex(1, (init(top->count) >> 0) & 0xF); // lower 4 bits
+        vbdPlot(int(top->count), 0, 255);
+        vbdCycle(i+1);
+        // end of Vbuddy output
+
         top->rst = (i<2) | (i==15); //assert reset for 2 cycles at start and at cycle 15
-        top->en = (i>=4);
+        top->en = vbdFlag(); //get enable signal from Vbuddy
         if (Verilated::gotFinish())  exit(0);
     }
+    vbdClose();
     tfp ->close();
     exit(0);
 }
